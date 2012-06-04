7948f9796cb45f9f477af640aa4ae96171a491ec
Berin Martini <berin.martini@gmail.com>
2012-06-04 17:14:41 -0400

    Move magic numbers into Camera __init
    Image size and some mask values.

--- src/Camera.lua
+++ src/Camera.lua
@@ -11,26 +11,36 @@ function Camera:__init(args)
    self.core = args.core
    self.msg_level = args.msg_level or 'none'  -- 'detailled' or 'none' or 'concise'
    self.frames = {}
+
+   self.nb_frames = 4 -- number of frames in running buffer
+   self.w_ = 640
+   self.h_ = 240
+   self.mask = {
+      ['counter'] = {['A'] = 0x00000003, ['B'] = 0x00030000},
+      ['status']  = {['A'] = 0x00000004, ['B'] = 0x00040000},
+      ['ctrl']    = {['A'] = 0x00000800, ['B'] = 0x08000000}
+   }
+
    -- compulsory
    if (self.core == nil) then
       error('<neuflow.Camera> ERROR: requires a Dataflow Core')
    end
 end
 
-function Camera:startCom(cameraID, alloc_frames)
+function Camera:initCamera(cameraID, alloc_frames)
 
    local buffer_size = alloc_frames.w * alloc_frames.h
 
    if cameraID == 'B' then
-      self.frames[2] = alloc_frames
+      self.frames['B'] = alloc_frames
       print('<neuflow.Camera> : init Camera B : alloc_frame ' .. alloc_frames.w .. 'x' .. alloc_frames.h .. ' at ' .. alloc_frames.x .. ' ' .. alloc_frames.y)
    else
-      self.frames[1] = alloc_frames
+      self.frames['A'] = alloc_frames
       print('<neuflow.Camera> : init Camera A : alloc_frame ' .. alloc_frames.w .. 'x' .. alloc_frames.h .. ' at ' .. alloc_frames.x .. ' ' .. alloc_frames.y)
    end
 
    local reg_ctrl = self.core.alloc_ur:get()
-   self.core:setreg(reg_ctrl, 0x00000000) -- Unset bit 16 of GPIO to 1
+   self.core:setreg(reg_ctrl, 0x00000000) -- Unset bit 16 of GPIO to 0
    self.core:iowrite(oFlower.io_gpios, reg_ctrl)
    self.core.alloc_ur:free(reg_ctrl)
 
@@ -51,15 +61,15 @@ function Camera:getLastFrame(cameraID)
    end
    for i = 1,#lcameraID do
       if lcameraID[i] == 'B' then
-	 if (mask_status == 0x00000000) or (mask_status == 0x00000004) then
-   	    mask_status = mask_status + 0x00040000
-	    table.insert(outputs, self.frames[2])
-   	 end
+         if (mask_status == 0x00000000) or (mask_status == self.mask.status['A']) then
+            mask_status = mask_status + self.mask.status['B']
+            table.insert(outputs, self.frames['B'])
+         end
       else
-	 if (mask_status == 0x00000000) or (mask_status == 0x00040000) then
-   	    mask_status = mask_status + 0x00000004
-	    table.insert(outputs, self.frames[1])
-   	 end
+         if (mask_status == 0x00000000) or (mask_status == self.mask.status['B']) then
+            mask_status = mask_status + self.mask.status['A']
+            table.insert(outputs, self.frames['A'])
+         end
       end
    end
    self.core:loopUntilStart()
@@ -73,9 +83,9 @@ function Camera:getLastFrame(cameraID)
 
    for i = 1,#lcameraID do
       if lcameraID[i] == 'B' then
-	 self.core:closePort(5)
+         self.core:closePort(dma.camera_B_port_id)
       else
-	 self.core:closePort(4)
+         self.core:closePort(dma.camera_A_port_id)
       end
    end
    return outputs
@@ -98,21 +108,21 @@ function Camera:captureOneFrame(cameraID)
    end
    for i = 1,#lcameraID do
       if lcameraID[i] == 'B' then
-   	 if (mask_ctrl == 0x00000000) or (mask_ctrl == 0x00000800) then
-   	    mask_ctrl = mask_ctrl + 0x08000000
-   	    self.core:openPortWr(5, self.frames[2])
-   	 end
-   	 if (mask_status == 0x00000000) or (mask_status == 0x00000004) then
-   	    mask_status = mask_status + 0x00040000
-   	 end
+         if (mask_ctrl == 0x00000000) or (mask_ctrl == self.mask.ctrl['A']) then
+            mask_ctrl = mask_ctrl + self.mask.ctrl['B']
+            self.core:openPortWr(5, self.frames['B'])
+         end
+         if (mask_status == 0x00000000) or (mask_status == self.mask.status['A']) then
+            mask_status = mask_status + self.mask.status['B']
+         end
       else
-   	 if (mask_ctrl == 0x00000000) or (mask_ctrl == 0x08000000) then
-   	    mask_ctrl = mask_ctrl + 0x00000800
-   	    self.core:openPortWr(4, self.frames[1])
-   	 end
-   	 if (mask_status == 0x00000000) or (mask_status == 0x00040000) then
-   	    mask_status = mask_status + 0x00000004
-   	 end
+         if (mask_ctrl == 0x00000000) or (mask_ctrl == self.mask.ctrl['B']) then
+            mask_ctrl = mask_ctrl + self.mask.ctrl['A']
+            self.core:openPortWr(4, self.frames['A'])
+         end
+         if (mask_status == 0x00000000) or (mask_status == self.mask.status['B']) then
+            mask_status = mask_status + self.mask.status['A']
+         end
       end
    end
 
@@ -137,8 +147,8 @@ end
 
 function Camera:enableCameras()
 
-   local w_ = 640
-   local h_ = 240
+   local w_ = self.w_
+   local h_ = self.h_
 
    print('<neuflow.Camera> Enable Camera: ' .. w_ .. 'x' .. h_)
    local idx = self.core.mem:allocOnTheHeap(h_, w_, nil, true)
@@ -146,6 +156,6 @@ function Camera:enableCameras()
    self.core.mem.buff[idx].id = idx
    self.core.mem.buff[idx2].id = idx2
 
-   self:startCom('A',self.core.mem.buff[idx])
-   self:startCom('B',self.core.mem.buff[idx2])
+   self:initCamera('A',self.core.mem.buff[idx])
+   self:initCamera('B',self.core.mem.buff[idx2])
 end
