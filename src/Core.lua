423e4b8361235580cdecaaefd719086b62581905
Berin Martini <berin.martini@gmail.com>
2012-06-25 17:57:43 -0400

    getStatus wait increased from 80 to 128
    This change is necessary to get the latest pico to work.

--- src/Core.lua
+++ src/Core.lua
@@ -774,7 +774,7 @@ function Core:getStatus(statusToGet)
    self:addInstruction {
       opcode = oFlower.op_getStatus,
       arg8_1 = statusToGet,
-      arg8_2 = 80
+      arg8_2 = 128
    }
 end
 
