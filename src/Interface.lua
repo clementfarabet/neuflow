clementfarabet/neuflow

commit c98ac9beb58e3e11ca31ba4201eaf3a7339d6ffb

Remove start/end process around stream to/from host

src/Interface.lua
@@ -46,17 +46,13 @@ function Ethernet:dev_copyToHost(tensor, ack)
    end
 
    for i = 1,#tensor do
-      self.core:startProcess()
       self:streamToHost(tensor[i], 'default', ack)
-      self.core:endProcess()
    end
 end
 
 function Ethernet:dev_copyFromHost(tensor)
    for i = 1,#tensor do
-      self.core:startProcess()
       self:streamFromHost(tensor[i], 'default')
-      self.core:endProcess()
    end
 
    -- always print a dummy flag, useful for profiling

src/DmaInterface.lua
@@ -44,23 +44,16 @@ end
 
 function DmaEthernet:dev_copyToHost(tensor)
    for i = 1, (#tensor-1) do
-      self.core:startProcess()
       self:streamToHost(tensor[i], 'default')
-
       self:streamFromHost(self.ack_stream[1], 'ack_stream')
-      self.core:endProcess()
    end
 
-   self.core:startProcess()
    self:streamToHost(tensor[#tensor], 'default')
-   self.core:endProcess()
 end
 
 function DmaEthernet:dev_copyFromHost(tensor)
    for i = 1,#tensor do
-      self.core:startProcess()
       self:streamFromHost(tensor[i], 'default')
-      self.core:endProcess()
    end
 end
 

commit 0bc3643e72c397a1eaaac96832bc38af4194c147

Bug Fix ml605 board introduced by c98ac9beb commit
Seems that the commands to stream to/from host are execution time sensitive for
the ml605 board and thus show be in a protected executionTimeSensitive code
block. This was only exposed when I trying the filter-back demo.

src/Interface.lua
@@ -46,13 +46,17 @@ function Ethernet:dev_copyToHost(tensor, ack)
    end
 
    for i = 1,#tensor do
-      self:streamToHost(tensor[i], 'default', ack)
+      self.core:executionTimeSensitive(function()
+         self:streamToHost(tensor[i], 'default', ack)
+      end)
    end
 end
 
 function Ethernet:dev_copyFromHost(tensor)
    for i = 1,#tensor do
-      self:streamFromHost(tensor[i], 'default')
+      self.core:executionTimeSensitive(function()
+         self:streamFromHost(tensor[i], 'default')
+      end)
    end
 
    -- always print a dummy flag, useful for profiling